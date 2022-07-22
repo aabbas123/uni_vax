48dbfae282d8065fa62440171d5e3a06691e1236
AliAsjad512 <aa0504503@gmail.com>
2022-07-22 11:23:32 -0400

    recent

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -22,7 +22,7 @@ router.post("/register", validInfo, async (req, res) => {
 
     try {
 
-        const { userName,email, phoneNumber, password, firstName, lastName, dateOfbirth, home, country,state,zip, genderType } = req.body;
+        const { userName, email, phoneNumber, password, firstName, lastName, dateOfbirth, home, country, state, zip, genderType } = req.body;
         //verify all the fields
         if (!email || !phoneNumber || !password || !firstName || !lastName || !dateOfbirth || !home || !country || !genderType) {
             return res.status(400).json({ message: "All input fields are required " })
@@ -37,7 +37,7 @@ router.post("/register", validInfo, async (req, res) => {
         // verify if user exists
         const user = await pool.query("SELECT * FROM users WHERE user_email =$1 AND user_phone =$2", [email, phoneNumber]);
 
-        console.log(user.rows);
+
         if (user.rows.length != 0) {
             return res.status(401).send("User already exist");
         }
@@ -48,11 +48,11 @@ router.post("/register", validInfo, async (req, res) => {
         const bcryptPassword = await bcrypt.hash(password, salt);
         const verificationToken = jwt.sign({ email: email }, process.env.VERFICATION_KEY, { expiresIn: "1hr" });
 
-        const newUser = await pool.query("INSERT INTO users (user_name,user_email,user_phone,user_password,verification_token,isverified) VALUES($1,$2,$3,$4,$5,$6) RETURNING *", [userName,email, phoneNumber, bcryptPassword, verificationToken, true]);
+        const newUser = await pool.query("INSERT INTO users (user_name,user_email,user_phone,user_password,verification_token,isverified) VALUES($1,$2,$3,$4,$5,$6) RETURNING *", [userName, email, phoneNumber, bcryptPassword, verificationToken, false]);
         //res.json(newUser.rows[0]);
         const user_id = newUser.rows[0].user_id;
-        const newprofile = await pool.query("INSERT INTO profile(first_name,last_name,date_of_birth,gender,home_address,country,state,zip_code,user_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *", [firstName, lastName, dateOfbirth, genderType, home, country,state,zip, user_id]);
-     
+        const newprofile = await pool.query("INSERT INTO profile(first_name,last_name,date_of_birth,gender,home_address,country,state,zip_code,user_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *", [firstName, lastName, dateOfbirth, genderType, home, country, state, zip, user_id]);
+
         const emailData = {
             from: process.env.EMAIL_USER,
             to: email,
@@ -60,10 +60,10 @@ router.post("/register", validInfo, async (req, res) => {
             html: `<h2>UNIVERSAL_VAX</h2>
     <p> Hi</p>
     <p> plz verify your account by clicking on this link</p>
-    <p>${process.env.CLIENT_URL}/auth/verify/${verificationToken}</p>  
+    <p>${process.env.CLIENT_URL}/verify/${verificationToken}</p>  
     `  }
 
-    let info = await transporter().sendMail(emailData);
+        let info = await transporter().sendMail(emailData);
         console.log(info.messageId);
         return res.status(200).json({ message: "Register successfully, verfication email has been sent" });
 
@@ -80,25 +80,32 @@ router.post("/register", validInfo, async (req, res) => {
 });
 
 
-router.get("/verify/:token",async(req,res)=>{
-    try{
+router.get("/verify/:token", async (req, res) => {
+    try {
         const { token } = req.params;
 
         let decoded = jwt.verify(token, process.env.VERFICATION_KEY);
         const { email } = decoded;
-        const user = await pool.query("SELECT * FROM users where user_email = $1 AND verification_token=$2",[email,token]);
-        if(user.rows.length===0){
-            return res.status(400).json({message:"invalid token"});
+        const verifiedUser = await pool.query("SELECT * FROM users where user_email = $1", [email]);
+       console.log('logs',verifiedUser.rows[0]);
+       if(verifiedUser.rows[0].isverified){
+           return res.status(400).json({message:"Account is verified"});
         }
-        const updateUser =await pool.query("UPDATE users SET isverified =$1,verification_token=$2 WHERE user_email =$3 AND verification_token=$4",[true,'',email,token]);
-        const tok = jwtGenerator(user.rows[0].user_id);
-        return res.json({ token:tok });
-        
+        const user = await pool.query("SELECT * FROM users where user_email = $1 AND verification_token=$2", [email, token]);
+        if (user.rows.length === 0) {
+            return res.status(400).json({ message: "Invalid Link" });
+        }
+        const updateUser = await pool.query("UPDATE users SET isverified =$1,verification_token=$2 WHERE user_email =$3 AND verification_token=$4", [true, '', email, token]);
+       //const tok = jwtGenerator(user.rows[0].user_id);
+       //return res.json({token:tok});
+       
+        return res.json({ message:"Your account has been verified, you can login now"});  
 
 
-    } catch(err){
+
+    } catch (err) {
         console.error(err.message);
-        res.status(500).send("Server error");
+        res.status(500).json({ message: "Link expired " });
     }
 })
 
@@ -120,12 +127,12 @@ router.post("/login", async (req, res) => {
         if (user.rows.length === 0) {
             return res.status(401).json(" incorrect credentials ");
         }
-          //verifiy the user
-        const checkIfverified=await pool.query("SELECT * from users WHERE user_email=$1 AND isverified=$2",[email,true]);
-         if(checkIfverified.rows.length === 0){
-            return res.status(403).json({message: "This user is not verified"})
-         }
-       // return res.status(403).json({message: ""})
+        //verifiy the user
+        const checkIfverified = await pool.query("SELECT * from users WHERE user_email=$1 AND isverified=$2", [email, true]);
+        if (checkIfverified.rows.length === 0) {
+            return res.status(403).json({ message: "This user is not verified" })
+        }
+        // return res.status(403).json({message: ""})
 
         //3 check if the incoming password is the same the database password
 
@@ -149,7 +156,7 @@ router.post("/login", async (req, res) => {
 
 
 
-router.get("/forgot", async (req, res) => {
+router.post("/forgot", async (req, res) => {
     try {
         const { email } = req.body;
         const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
@@ -167,7 +174,7 @@ router.get("/forgot", async (req, res) => {
             html: `<h2>UNIVERSAL_VAX</h2>
             <p> Hi</p>
             <p> Follow the link below to reset your password</p>
-            <p>${process.env.CLIENT_URL}/auth/reset/${token}</p>  
+            <p>${process.env.CLIENT_URL}/reset/${token}</p>  
             `  }
 
         let info = await transporter().sendMail(emailData);
@@ -189,10 +196,10 @@ router.get("/reset/:token", async (req, res) => {
 
         let decoded = jwt.verify(token, process.env.PASSWORD_RESET_KEY);
         const { id, email } = decoded;
-        return res.status(200).json({ message: "token is Valid" });
+        return res.status(200).json({ message: "Link is not Valid" });
     } catch (err) {
         console.log(err.message);
-        res.status(500).json({ message: "Server error" });
+        res.status(500).json({ message: "Invalid Link" });
 
     }
 
