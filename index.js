ef0558bdc0bf239ab01d82dadfb59caad552131c
AliAsjad512 <aa0504503@gmail.com>
2022-07-27 09:22:19 -0400

    new2

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+
 app.use(express.json());
-app.use(cors());
+app.use(cors());    
 app.use("/auth",require("./routes/jwtAuth"));
 app.use("/dashboard",require("./routes/dashboard"));
 app.use("/users",require("./routes/users"));
-
-app.listen(5000, () => {
-    console.log("Server is runnong on port 5000")
+     
+const port = process.env.PORT || 5000;
+app.listen(port,() => {
+    console.log(`Server is runnong on port ${port}`);
 })
