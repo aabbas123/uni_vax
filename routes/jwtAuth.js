a676adb86ebf24bc7a0d224660d4c2f6fdd59202
AliAsjad512 <aa0504503@gmail.com>
2022-08-01 10:20:51 -0400

    new7

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -73,7 +73,7 @@ router.post("/register", validInfo, async (req, res) => {
 
     } catch (err) {
         console.error(err)
-        res.status(500).send("Server error")
+        res.status(500).json({message:"Server error"});
     }
 
 
