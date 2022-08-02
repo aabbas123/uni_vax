33e5c58888dcfbbb372c316ef05b648837d7c709
AliAsjad512 <aa0504503@gmail.com>
2022-08-02 09:07:36 -0400

    new9

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -40,7 +40,7 @@ router.post("/register", validInfo, async (req, res) => {
         // verify if user exists
         const user = await pool.query("SELECT * FROM users WHERE user_email =$1 AND user_phone =$2", [email, phoneNumber]);
 
-        console.log(user.rows);
+     //   console.log(user.rows);
         if (user.rows.length != 0) {
             return res.status(401).json({message:"User already exist"});
         }
@@ -51,10 +51,10 @@ router.post("/register", validInfo, async (req, res) => {
         const bcryptPassword = await bcrypt.hash(password, salt);
         const verificationToken = jwt.sign({ email: email }, process.env.VERFICATION_KEY, { expiresIn: "1hr" });
 
-       // const newUser = await pool.query("INSERT INTO users (user_name,user_email,user_phone,user_password,verification_token,isverified) VALUES($1,$2,$3,$4,$5,$6) RETURNING *", [userName, email, phoneNumber, bcryptPassword, verificationToken, false]);
+        const newUser = await pool.query("INSERT INTO users (user_name,user_email,user_phone,user_password,verification_token,isverified) VALUES($1,$2,$3,$4,$5,$6) RETURNING *", [userName, email, phoneNumber, bcryptPassword, verificationToken, false]);
         //res.json(newUser.rows[0]);
-        //const user_id = newUser.rows[0].user_id;
-       // const newprofile = await pool.query("INSERT INTO profile(first_name,last_name,date_of_birth,gender,home_address,country,state,zip_code,user_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *", [firstName, lastName, dateOfbirth, genderType, home, country, state, zip, user_id]);
+        const user_id = newUser.rows[0].user_id;
+       const newprofile = await pool.query("INSERT INTO profile(first_name,last_name,date_of_birth,gender,home_address,country,state,zip_code,user_id) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *", [firstName, lastName, dateOfbirth, genderType, home, country, state, zip, user_id]);
 
         const emailData = {
             from: process.env.EMAIL_USER,
